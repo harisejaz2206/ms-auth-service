311577b05136443faa58a70ecf828793d0050495
Haris Ejaz <harisejaz2206@gmail.com>
2025-02-21 18:46:55 +0500

    feat: added express server config

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,16 @@
+import app from './app'
 import { Config } from './config'
 
-console.log(Config.PORT)
+const startServer = () => {
+  try {
+    const PORT = Config.PORT
+    app.listen(PORT, () =>
+      console.log(`🚀 Server is up and running on port ${PORT}`),
+    )
+  } catch (error) {
+    console.error('❌ Failed to start the server:', error)
+    process.exit(1)
+  }
+}
+
+startServer()
